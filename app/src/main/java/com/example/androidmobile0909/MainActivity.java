bfe79afa2efebe16b8fff232f51bc31063a0c235
Doug0506 <78805196+Doug0506@users.noreply.github.com>
2022-09-06 20:34:17 -0300

    Aplicando o conceito da Tela
    Widget com a tela de localização

--- app/src/main/java/com/example/androidmobile0909/MainActivity.java
+++ app/src/main/java/com/example/androidmobile0909/MainActivity.java
@@ -1,21 +1,31 @@
 package com.example.androidmobile0909;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
+import android.Manifest;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
+import android.webkit.WebView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity{
 
-    Button btnEnviarNome, btnOndeEstou;
+    Button btnEnviarNome;
     EditText textNome;
-    TextView textLatitude, textLongetude;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,11 +34,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btnEnviarNome = findViewById(R.id.btnEnviarNome);
-        btnOndeEstou = findViewById(R.id.btnOndeEstou);
-
         textNome = findViewById(R.id.textNome);
-        textLatitude = findViewById(R.id.TextLatitute);
-        textLongetude = findViewById(R.id.TextLongetude);
+
+
 
         btnEnviarNome.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -41,6 +49,38 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    public void buscarInformacoesGPS(View v) {
 
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)   != PackageManager.PERMISSION_GRANTED &&
+                ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(MainActivity.this, new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            ActivityCompat.requestPermissions(MainActivity.this, new String[] {Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
+            ActivityCompat.requestPermissions(MainActivity.this, new String[] {Manifest.permission.ACCESS_NETWORK_STATE}, 1);
+            return;
+        }
+
+        LocationManager  mLocManager  = (LocationManager) getSystemService(MainActivity.this.LOCATION_SERVICE);
+        LocationListener mLocListener = new LocalizacaoListener();
+
+        mLocManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, mLocListener);
+
+        if (mLocManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+            String texto = "Latitude.: " + LocalizacaoListener.latitude + "\n" +
+                    "Longitude: " + LocalizacaoListener.longitude + "\n";
+            Toast.makeText(MainActivity.this, texto, Toast.LENGTH_LONG).show();
+        } else {
+            Toast.makeText(MainActivity.this, "GPS DESABILITADO.", Toast.LENGTH_LONG).show();
+        }
+
+        this.mostrarGoogleMaps(LocalizacaoListener.latitude, LocalizacaoListener.longitude);
+    }
+
+    public void mostrarGoogleMaps(double latitude, double longitude) {
+        WebView wv = findViewById(R.id.webv);
+        wv.getSettings().setJavaScriptEnabled(true);
+        wv.loadUrl("https://www.google.com/maps/search/?api=1&query=" + latitude + "," + longitude);
+    }
 
 }
+
